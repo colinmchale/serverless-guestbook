d847f6ac4a6d405702756d938875df15d099a00b
colinmchale <colin.mchale22@gmail.com>
2023-02-23 16:55:10 -0600

    added apiURL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/6a533c4f-9530-43e1-8804-8a5caa467f20/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
